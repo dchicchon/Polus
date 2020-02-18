2235214db525da6031cd7b7a7e821216b5bac7e2
dchicchon <danielchicchon@gmail.com>
2020-02-17 23:43:31 -0800

    add query in search parameter for background.js

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
-
 // Change background photo every day
 let url =
-  "https://api.unsplash.com/photos/random/?client_id=fdf184d2efd7efc38157064835198f0ce7d9c4f7bfcec07df0d9e64378a8d630";
+  "https://api.unsplash.com/photos/random/?client_id=fdf184d2efd7efc38157064835198f0ce7d9c4f7bfcec07df0d9e64378a8d630&query=nature";
 fetch(url)
   .then(function(result) {
     return result.json();
   })
   .then(function(photo) {
+    console.log(photo);
     chrome.storage.sync.set({ background: photo.urls.full }, function(result) {
       console.log("New Background:", photo.urls.full);
     });
