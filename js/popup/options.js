1e2d6b8f537906d3391861281137a7dfa8f3b16b
dchicchon <danielchicchon@gmail.com>
2021-01-07 23:38:44 -0800

    add feature to select photo for background

--- js/popup/options.js
+++ js/popup/options.js
@@ -32,8 +32,34 @@ for (let toggle of toggles) {
 }
 
 document.getElementById("submitPhoto").onclick = function () {
-    let url = document.getElementById("photoURL").value
+    let photoURL = document.getElementById("photoURL").value
     console.log("Submitted photo!")
-    console.log(url)
+    let arr = photoURL.split('/')
+    console.log(arr)
+    let id = arr[arr.length - 1]
+    console.log(id)
+    let requestPhotoURL =
+        `https://api.unsplash.com/photos/${id}/?client_id=fdf184d2efd7efc38157064835198f0ce7d9c4f7bfcec07df0d9e64378a8d630&`;
+    console.log(requestPhotoURL)
+    fetch(requestPhotoURL, { mode: 'cors', credentials: 'omit' })
+        .then(response => {
+            if (!response.ok) throw response.statusText;
+            return response
+        })
+        .then(response => response.json())
+        .then(function (photo) {
+            let url = photo.urls.raw;
+            let location = photo.location.name ? `${photo.location.name}` : "Unknown";
+            let author = photo.user.name ? `${photo.user.name}` : "Unknown";
+            let photoLink = photo.links.html;
+            let downloadLink = `https://unsplash.com/photos/${photo.id}/download?client_id=fdf184d2efd7efc38157064835198f0ce7d9c4f7bfcec07df0d9e64378a8d630&force=true`;
+            chrome.storage.sync.set({
+                background: { url, location, author, photoLink, downloadLink },
+            });
+        })
+        .catch((err) => console.log(`Fetch failed: ${err}`));
 }
 
+chrome.storage.onChanged.addListener(function (result) {
+    console.log(result)
+})
