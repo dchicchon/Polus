5045d0305cbdd0466029f67726ffbeeed38dc847
dchicchon <danielchicchon@gmail.com>
2021-07-05 18:41:45 -0400

    add login and signup pages to app, also figured out auth

---
+++ app/lib/auth.dart
@@ -0,0 +1,36 @@
+// import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger auth flow
+  final GoogleSignInAccount googleUser = await GoogleSignIn().signIn();
+
+  // obtain auth details from the request
+  final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+  // create a new credential
+  final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken, idToken: googleAuth.idToken);
+
+  // once signed in, return the user credential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+Future<UserCredential> createWithEmailAndPassword(email, password) async {
+  try {
+    UserCredential userCredential = await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    return userCredential;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print("The password provided is too weak");
+    } else if (e.code == 'email-already-in-use') {
+      print("The account already exists for that email");
+    }
+  } catch (e) {
+    print(e);
+  }
+}
+
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:firebase_auth/firebase_auth.dart';
+import 'auth.dart';
 
 // Starting FlutterFire through the suggestion found here
 // https://firebase.flutter.dev/docs/overview
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
@@ -17,6 +19,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  User _user;
   bool _initialized = false;
   bool _error = false;
 
@@ -48,14 +51,33 @@ class _MyAppState extends State<MyApp> {
           home: SomethingWentWrong());
     }
 
+// change this to its own thing late
     if (!_initialized) {
-      return Loading();
+      return MaterialApp(
+          title: "loading",
+          theme: ThemeData(primarySwatch: Colors.blue),
+          home: Loading());
     }
 
     return MaterialApp(
         title: "Polus",
         theme: ThemeData(primarySwatch: Colors.blue),
-        home: MyAwesomeApp());
+        home: Navigator(
+          pages: [
+            MaterialPage(
+                key: ValueKey("Auth Page"), child: Auth(onLogin: _handleLogin)),
+            if (_user != null)
+              MaterialPage(key: ValueKey("Home Page"), child: HomePage())
+          ],
+          onPopPage: (route, result) => route.didPop(result),
+        ));
+  }
+
+  void _handleLogin(User user) {
+    print("Handle Login");
+    setState(() {
+      _user = user;
+    });
   }
 }
 
@@ -88,20 +110,293 @@ class Loading extends StatelessWidget {
 }
 
 // This is where we will check if there is a user logged in or not
+class Auth extends StatefulWidget {
+  final Function onLogin;
 
-class MyAwesomeApp extends StatefulWidget {
-  MyAwesomeApp({Key key}) : super(key: key);
+  Auth({Key key, this.onLogin}) : super(key: key);
 
   @override
-  _MyAwesomeAppState createState() => _MyAwesomeAppState();
+  _AuthState createState() => _AuthState();
 }
 
-class _MyAwesomeAppState extends State<MyAwesomeApp> {
+class _AuthState extends State<Auth> {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  // Only show this if user is not signedin
+  String authWidget = 'login';
+
+  Widget whichWidget() {
+    if (this.authWidget == 'login') {
+      return Login();
+    } else {
+      return SignUp();
+    }
+  }
+
   @override
+  void initState() {
+    firebaseAuth.authStateChanges().listen((User user) {
+      widget.onLogin(user);
+      // if (user == null) {
+      //   print("User is signed out");
+      //   widget.onLogin(user)
+      // } else {
+      //   // Maybe navigate to homePage?
+      //   print("User is signed in, changing login state");
+      //   widget.onLogin(user);
+      // }
+    });
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Polus")),
-      body: const Center(child: Text("Hello World")),
+      body: Center(
+          child: Container(
+              color: Colors.blue[100],
+              width: 325,
+              height: 275,
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Text(
+                    (this.authWidget == 'login' ? "Login" : "Sign Up"),
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  whichWidget(),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (this.authWidget == 'login') {
+                          this.setState(() {
+                            this.authWidget = 'signup';
+                          });
+                        } else {
+                          this.setState(() {
+                            this.authWidget = 'login';
+                          });
+                        }
+                      },
+                      child: Text((this.authWidget == 'login'
+                          ? 'Dont have an account? Sign up here'
+                          : 'Already have an account? Click here')))
+                ],
+              ))),
     );
+  }
+}
+
+class Login extends StatefulWidget {
+  Login({Key key}) : super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final GlobalKey<FormState> _loginKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  String email = '';
+  String password = '';
+
+  @override
+  void initState() {
+    print("Starting Login");
+    super.initState();
+  }
+
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Form(
+        key: _loginKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(hintText: 'Enter your email'),
+              validator: (String value) {
+                if (value == null || value.isEmpty) {
+                  return "Please Enter Some Text";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: passwordController,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your password'),
+              validator: (String value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please Enter a valid password';
+                }
+                return null;
+              },
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            print("Submitting Credentials");
+                            if (_loginKey.currentState.validate()) {
+                              print("Valid form");
+                              // Do work here
+                            } else {
+                              print("Form not valid");
+                            }
+                          },
+                          child: const Text("Submit")),
+                    )
+                  ],
+                ),
+                SizedBox(width: 25),
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print("Login w/ Google");
+                        },
+                        child: Text("Google"),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ));
+  }
+}
+
+class SignUp extends StatefulWidget {
+  SignUp({Key key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final GlobalKey<FormState> _signUpKey = GlobalKey<FormState>();
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  String email = '';
+  String password = '';
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
+    return Form(
+        key: _signUpKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(hintText: 'Enter your email'),
+              validator: (String value) {
+                if (value == null || value.isEmpty) {
+                  return "Please Enter Some Text";
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: passwordController,
+              decoration:
+                  const InputDecoration(hintText: 'Enter your password'),
+              validator: (String value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please Enter a valid password';
+                }
+                return null;
+              },
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            final form = _signUpKey.currentState;
+                            // Do work here
+                            if (form.validate()) {
+                              print(emailController.text);
+                              print(passwordController.text);
+                              createWithEmailAndPassword(emailController.text,
+                                  passwordController.text);
+                            } else {
+                              print("Form not valid");
+                            }
+                          },
+                          child: const Text("Submit")),
+                    )
+                  ],
+                ),
+                SizedBox(width: 25),
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print("SignUp w/ Google");
+                        },
+                        child: Text("Google"),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Center(
+            child: Container(
+                child: Column(
+      children: [
+        Text('You are logged in!'),
+        ElevatedButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: Text("Logout")),
+      ],
+    ))));
   }
 }
