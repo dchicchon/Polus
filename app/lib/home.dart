652cf1da4815271345c119c036ffd0c27dafbe0f
dchicchon <danielchicchon@gmail.com>
2021-07-06 18:45:48 -0400

    still working on adding entries to firestore. Working on function addEntry

--- app/lib/home.dart
+++ app/lib/home.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -12,8 +10,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<String> entries = <String>['A', 'B', 'C'];
-  List<int> colorCodes = <int>[600, 500, 100];
+  // List<String> entries = <String>['A', 'B', 'C'];
+  // List<int> colorCodes = <int>[600, 500, 100];
 
   // By default, look at todays date
   // show loading symbol while getting entries
@@ -21,6 +19,38 @@ class _HomePageState extends State<HomePage> {
   // Get a list of entries from our database
 
   // List<Map> getEntries() {}
+  Future<void> addEntry({String date = '7/6/2021'}) async {
+    print("Adding Entry");
+
+    // Document reference
+    DocumentReference userRef = FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser.uid);
+
+    // get data
+    dynamic data = await userRef.get();
+
+    // set date (will do in parameters later)
+
+    // Get our list of entries
+    List entryList = data.data()[date];
+
+    Map<String, dynamic> entry = {'text': 'Hello World'};
+
+    entryList.add(entry);
+
+    print(entryList);
+
+    // userRef
+    //     .set({'text': "Hello"})
+    //     .then((value) => print('success'))
+    //     .catchError((err) => print(err));
+    // Fieldpath field = FieldPath.of(date);
+    // userRef
+    //     .update()
+    //     .then((value) => print("Entry List Updated"))
+    //     .catchError((error) => print("Failed to update list $error"));
+  }
 
   @override
   void initState() {
@@ -30,15 +60,18 @@ class _HomePageState extends State<HomePage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Polus"),
-          actions: [
-            IconButton(
-                onPressed: FirebaseAuth.instance.signOut,
-                icon: Icon(Icons.navigate_next)),
-          ],
-        ),
-        body: Container(child: Center(child: EntriesList())));
+      appBar: AppBar(
+        title: const Text("Polus"),
+        actions: [
+          IconButton(
+              onPressed: FirebaseAuth.instance.signOut,
+              icon: Icon(Icons.navigate_next)),
+        ],
+      ),
+      body: Container(child: Center(child: EntriesList())),
+      floatingActionButton: FloatingActionButton(
+          onPressed: addEntry, tooltip: 'Increment', child: Icon(Icons.add)),
+    );
   }
 }
 
@@ -56,32 +89,29 @@ class _EntriesListState extends State<EntriesList> {
       .snapshots();
 
 // https://stackoverflow.com/questions/66074484/type-documentsnapshot-is-not-a-subtype-of-type-mapstring-dynamic
-  List<Widget> getEntries(snapshot, dateStamp) {
-    print(snapshot.data
-        .data()); // this returns all of the data I have in my database
+  List<Widget> getEntries(snapshot, {String dateStamp = '7/6/2021'}) {
+    print("Get Entries");
 
-    List myList = snapshot.data.data()['7/6/2021'].map<Widget>((entry) {
-      print("ENTRY");
-      print(entry);
+    // Our entry list based on datestamp
+    List entryList = snapshot.data.data()[dateStamp];
 
-      return ListTile(title: Text(entry['text']));
+    // Check if data is empty
+    if (entryList == null) {
+      return [];
+    }
+
+    // If not empty, return a list of ListTiles widgets with the data we got
+    List myList = entryList.map<Widget>((entry) {
+      return ListTile(
+        title: Text(
+          entry['text'],
+          style: TextStyle(color: Colors.white),
+        ),
+        tileColor: Color.fromRGBO(21, 115, 170, 0.80),
+      );
     }).toList();
 
-    print(myList);
-
     return myList;
-
-    // Map<String, dynamic> map = Map<String, dynamic>.from(snapshot.data.data()['7/6/2021']);
-
-    // print("GET DATE");
-    // print(map);
-
-    // return snapshot.data.data().map((entry) {
-    //   Map<String, dynamic> data = entry as Map<String, dynamic>;
-    //   return ListTile(title: Text(data['text']));
-    // }).toList();
-
-    // return Text("Hello");
   }
 
   @override
@@ -96,14 +126,7 @@ class _EntriesListState extends State<EntriesList> {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return CircularProgressIndicator();
         }
-        return ListView(children: getEntries(snapshot, null));
-        // return ListView(children: [Text('${snapshot.data}')]);
-
-        // return ListView(
-        //     children: snapshot.data['7-6-2021'].map((k, v) {
-
-        //   return ListTile(title: Text(v));
-        // }).toList());
+        return ListView(children: getEntries(snapshot));
       },
     );
   }
@@ -111,21 +134,20 @@ class _EntriesListState extends State<EntriesList> {
 
 // snapshot.data.map((DocumentSnapshot document) {
 //             Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-//             return ListTile(  
+//             return ListTile(
 //               title: Text(data['text']),
 //               subtitle: Text(data['color']),
 //             );
 //           }).toList(),
 
-
-      //  ListView.separated(
-      //     padding: EdgeInsets.all(8),
-      //     itemCount: entries.length,
-      //     itemBuilder: (BuildContext context, int index) {
-      //       return Container(
-      //           child: Center(child: Text('Entry ${entries[index]}')),
-      //           height: 50,
-      //           color: Colors.blue[colorCodes[index]]);
-      //     },
-      //     separatorBuilder: (BuildContext context, int index) => Divider(),
-      //   )
+//  ListView.separated(
+//     padding: EdgeInsets.all(8),
+//     itemCount: entries.length,
+//     itemBuilder: (BuildContext context, int index) {
+//       return Container(
+//           child: Center(child: Text('Entry ${entries[index]}')),
+//           height: 50,
+//           color: Colors.blue[colorCodes[index]]);
+//     },
+//     separatorBuilder: (BuildContext context, int index) => Divider(),
+//   )
