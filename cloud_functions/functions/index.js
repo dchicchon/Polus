bb59350ad45f100e7af61a8b954dba793409d50a
dchicchon <danielchicchon@gmail.com>
2021-08-12 18:08:41 -0400

    add delete task function to cloud functions

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -12,6 +12,9 @@ const join = (t, s) => {
     return a.map(format).join(s);
 }
 
+// This article saved me tons of research by showing how to use Cloud Tasks. I then implemented Cloud Firebase Notifications
+// https://medium.com/firebase-developers/how-to-schedule-a-cloud-function-to-run-in-the-future-in-order-to-build-a-firestore-document-ttl-754f9bf3214a
+
 // Potentially do this
 // exports.checkForTasks = functions.pubsub.schedule('0 0 * * *').onRun((context) => {
 //     const date = join(new Date(), '-')
@@ -31,18 +34,15 @@ const join = (t, s) => {
 // })
 
 exports.createTask = functions.https.onCall(async (data, context) => {
-    log("create task")
+    // I should somehow check to users current timezone? just so we know which date they want to edit
+    log("Create task")
+    let { time, date, uid, id, } = data
     const taskClient = new CloudTasksClient();
-    let { time, uid, id } = data
-    // log(time)
-    // log(uid)
-    // log(id)
-    // Check if task already exists in entry
     // Get Date from time in format mm-dd-yyyy
-
-    // Reason why this sucks is because I am using UTC
-    let entryDate = new Date(time[0], time[1], time[2], time[3], time[4],);
-    const date = join(entryDate, '-');
+    log(time)
+    let entryDate = new Date(time);
+    const expirationAtSeconds = (entryDate.getTime() + Date.now()) / 1000 // offset in seconds
+    log(expirationAtSeconds)
     let prevEntry = await admin.firestore().doc(`/users/${uid}/${date}/${id}`).get()
     let prevEntryData = await prevEntry.data()
     // log(prevEntryData)
@@ -50,17 +50,6 @@ exports.createTask = functions.https.onCall(async (data, context) => {
         // Delete old expiration task
         await taskClient.deleteTask({ name: prevEntryData.expirationTask })
     }
-    log(date)
-    // This works now! Now I should create a task on google tasks
-    const todayDate = new Date()
-
-    log(entryDate.getTime())
-    log(todayDate.getTime())
-
-    const expirationAtSeconds = (entryDate.getTime() - todayDate.getTime()) / 1000 // offset in seconds
-
-    log(`Expire in:`)
-    log(expirationAtSeconds)
 
     const project = JSON.parse(process.env.FIREBASE_CONFIG).projectId
     const location = 'us-central1'
@@ -87,19 +76,28 @@ exports.createTask = functions.https.onCall(async (data, context) => {
     }
     const [response] = await taskClient.createTask({ parent: queuePath, task })
     const expirationTask = response.name;
-    const update = { expirationTask }
     // update the entry with the expiration task name
-    await admin.firestore().doc(docPath).update(update)
+    await admin.firestore().doc(docPath).update({ expirationTask })
     log("Done with Create Task")
-    return ['Success!']
+    return ['Success!', expirationAtSeconds]
 })
 
 // If entry had a running task, then we will call this function
-// exports.deleteTask = functions.https.onCall(async (data, context) => {
-//     const taskClient = new CloudTasksClient();
-//     const { expirationTask } = data
-//     await taskClient.deleteTask({ name: expirationTask })
-// })
+exports.deleteTask = functions.https.onCall(async (data, context) => {
+    log("Delete Task")
+    try {
+        const taskClient = new CloudTasksClient();
+        const { date, uid, id } = data
+        let entry = await admin.firestore().doc(`/users/${uid}/${date}/${id}`).get()
+        let { expirationTask } = entry.data();
+        await taskClient.deleteTask({ name: expirationTask })
+        return ['Successfully Deleted Data']
+    } catch (e) {
+        log("Something Went Wrong")
+        log(e)
+        return ['Something Went Wrong!']
+    }
+})
 // // on deleting an entry, we should also delete the task as well on googlecloud client
 
 exports.firestoreTtlCallback = functions.https.onRequest(async (req, res) => {
@@ -107,10 +105,23 @@ exports.firestoreTtlCallback = functions.https.onRequest(async (req, res) => {
         const payload = req.body;
         let entry = await (await admin.firestore().doc(payload.docPath).get()).data();
         let tokens = await (await admin.firestore().doc(`/users/${payload.uid}`).get()).get('tokens')
-        log(entry);
-        log(tokens)
-        // send firebase messaging to all devices!
-        // admin.messaging().sendToDevice('token')
+        await admin.messaging().sendMulticast({
+            tokens,
+            notification: {
+                title: "Polus",
+                body: entry['text']
+            }
+        }).then((response) => {
+            log('Successfully sent message:')
+            log(response)
+            const taskClient = new CloudTasksClient();
+            let { expirationTask } = admin.firestore().doc(payload.docPath).get()
+            await taskClient.deleteTask({ name: expirationTask })
+            admin.firestore().doc(payload.docPath).update({ expirationTask: admin.firestore.FieldValue.delete() })
+        }).catch((error) => {
+            log('Error in sending Message')
+            log(error)
+        })
         res.status(200)
     } catch (err) {
         log(err)
