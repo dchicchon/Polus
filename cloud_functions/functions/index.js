55765cea70a8cdc247c7cc699ac94a00c7f33337
dchicchon <danielchicchon@gmail.com>
2021-08-13 12:59:13 -0400

    add config file to this directory to use in index.js in config

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,65 +1,41 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin')
 const { CloudTasksClient } = require('@google-cloud/tasks');
-admin.initializeApp()
+const config = require('../serviceAccountKey.json')
+admin.initializeApp(config)
 const log = functions.logger.log
-const join = (t, s) => {
-    let a = [{ month: 'numeric' }, { day: 'numeric' }, { year: 'numeric' }];
-    function format(m) {
-        let f = new Intl.DateTimeFormat('en', m);
-        return f.format(t);
-    }
-    return a.map(format).join(s);
-}
 
 // This article saved me tons of research by showing how to use Cloud Tasks. I then implemented Cloud Firebase Notifications
 // https://medium.com/firebase-developers/how-to-schedule-a-cloud-function-to-run-in-the-future-in-order-to-build-a-firestore-document-ttl-754f9bf3214a
 
-// Potentially do this
-// exports.checkForTasks = functions.pubsub.schedule('0 0 * * *').onRun((context) => {
-//     const date = join(new Date(), '-')
-//     const users = admin.firestore().getAll()
-//     log(users)
-//     // for user in users
-//     // check if user has date collection
-//     // check if user has any entries that are timed for that collection
-//     // create task for each timed entry
-
-// })
-
-// exports.test = functions.https.onCall(async (data, context) => {
-//     const date = join(new Date(), '-')
-//     const users = admin.firestore().collection('users').get()
-//     log(users)
-// })
-
 exports.createTask = functions.https.onCall(async (data, context) => {
     // I should somehow check to users current timezone? just so we know which date they want to edit
     log("Create task")
-    let { difference, date, uid, id, } = data
+    const { difference, date, uid, id, } = data
     const taskClient = new CloudTasksClient();
-    log(difference)
+    const expirationAtSeconds = difference + (Date.now() / 1000) // offset in seconds
 
-    const expirationAtSeconds = difference + Date.now() / 1000 // offset in seconds
-
-    let prevEntry = await admin.firestore().doc(`/users/${uid}/${date}/${id}`).get()
+    const docPath = `/users/${uid}/${date}/${id}`
+    let prevEntry = await admin.firestore().doc(docPath).get()
     let prevEntryData = await prevEntry.data()
-    // log(prevEntryData)
     if (prevEntryData.hasOwnProperty('expirationTask')) {
         // Delete old expiration task
         await taskClient.deleteTask({ name: prevEntryData.expirationTask })
     }
+    // This is good, its something wrong with firebase env?
 
-    const project = JSON.parse(process.env.FIREBASE_CONFIG).projectId
+    const project = 'polus-cc376'
+    log(project)
     const location = 'us-central1'
     const queue = 'firestore-ttl'
     const queuePath = taskClient.queuePath(project, location, queue)
+    log(queuePath)
     const url = `https://${location}-${project}.cloudfunctions.net/firestoreTtlCallback`
-    const docPath = `/users/${uid}/${date}/${id}`
     const payload = {
         docPath,
         uid,
     }
+    log("Payload")
     log(payload)
     const task = {
         httpRequest: {
