5298f6d93be8cb63ccbaab789fadcf65b45d1854
Daniel Chicchon <danielchicchon@gmail.com>
2022-11-01 08:00:51 -0700

    remove scss file in favor of just having one file.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,13 +1,7 @@
 import { createApp } from "vue";
 import Popup from "./Popup.vue";
-// import "vue-material/dist/vue-material.min.css";
-// import "vue-material/dist/theme/default.css";
-// import { MdField, MdButton, MdBottomBar } from "vue-material/dist/components";
 import { actions } from "../utils/store";
 
-// Vue.use(MdField);
-// Vue.use(MdButton);
-// Vue.use(MdBottomBar);
 const start = async () => {
   await actions.initUserSettings()
   const popup = createApp(Popup).mount('#popup')
