2ec37b6e05a2a45bcfec9926077e5ce71c1472be
dchicchon <danielchicchon@gmail.com>
2023-04-17 13:05:02 -0700

    ensure that indexOpen and newtab false work

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,37 +2,17 @@ import { render } from 'preact';
 import { actions, userSettings } from '../utils/index.jsx';
 import App from './App.jsx';
 
-
 window.onload = async () => {
-  await actions.initializeBackground();
   await actions.initializeUserSettings();
-  console.log(userSettings.value)
-  // check the user settings?
+  await actions.initializeBackground();
   if (userSettings.value.newTab || userSettings.value.indexOpen) {
+    if (userSettings.value.indexOpen) {
+      userSettings.value = {
+        ...userSettings.value,
+        indexOpen: false
+      }
+    }
     return render(<App />, document.getElementById('app'));
   }
   actions.showDefaultTab();
-  
 };
-
-// import { createApp } from "vue";
-// import App from "./App.vue";
-// import { state, actions } from '../utils'
-
-// const startApp = async () => {
-//   console.info('Start App')
-//   await actions.initBackground()
-//   await actions.initUserSettings()
-//   if (state.userSettings.newTab || state.userSettings.indexOpen) {
-//     // need to do this if I want functionality for new open tab
-//     state.userSettings.indexOpen = false;
-//     await actions.setUserSettings()
-//     createApp(App).mount('#app');
-//   } else {
-//     actions.showDefaultTab()
-//   }
-// };
-
-// window.onload = () => {
-//   startApp();
-// };
